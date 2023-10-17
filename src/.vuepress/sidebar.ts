45ec0bb951006b3f933260c229a5862e9b371ee5
Summer <summer@tradev-group.com>
2023-10-17 10:49:50 +0800

    feat: update sidebar
    add new side item and add new sidebar icons

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -4,18 +4,34 @@ export default sidebar({
   "/": [
     "",
     {
-      text: "案例",
+      text: "编程语言与技巧",
       icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
+      link: "Coding/",
     },
     {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
+      text: "计算机科学与技术",
+      icon: "https://api.iconify.design/teenyicons/computer-outline.svg",
+      link: "ComputerScience/",
     },
-    "slides",
+    {
+      text: "代码最佳实践",
+      icon: "https://api.iconify.design/medical-icon/i-family-practice.svg",
+      link: "BestPractice/",
+    },
+    {
+      text: "英语",
+      icon: "https://api.iconify.design/icon-park-outline/english.svg",
+      link: "English/",
+    },
+    {
+      text: "数学",
+      icon: "https://api.iconify.design/bx/math.svg",
+      link: "Math/",
+    },
+    {
+      text: "个人",
+      icon: "https://api.iconify.design/icon-park-outline/personal-privacy.svg",
+      link: "Individual/",
+    },
   ],
 });
